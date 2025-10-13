d9a0520e4b004089df7df50625fc73dcf89bd3ff
Your Name <you@example.com>
2025-10-13 20:15:57 +0700

    feat: s3 module

--- development/s3.tf
+++ development/s3.tf
@@ -18,3 +18,16 @@ resource "aws_s3_bucket_lifecycle_configuration" "this" {
     }
   }
 }
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "development-trieu-policy"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
