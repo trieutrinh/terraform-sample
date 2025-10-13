3c240a28ba9d38a51cceb4ca4b17b0ab9ef01b29
Your Name <you@example.com>
2025-10-13 20:23:17 +0700

    feat: add policy

--- development/s3.tf
+++ development/s3.tf
@@ -25,9 +25,47 @@ module "s3_bucket" {
   acl    = "private"
 
   control_object_ownership = true
-  object_ownership         = "ObjectWriter"
+  object_ownership         = "BucketOwnerPreferred"
+  attach_policy            = true
+  policy                   = data.aws_iam_policy_document.bucket_policy.json
 
   versioning = {
     enabled = true
   }
 }
+
+resource "aws_iam_role" "this" {
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_role.this.arn]
+    }
+
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      "${module.s3_bucket.bucket_arn}",
+    ]
+  }
+}
+
